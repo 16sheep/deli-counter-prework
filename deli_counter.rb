d099476c34e6e4b998a33f42d5952073ba523de0
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-09 09:06:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.length < 0
+  if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
     first_in_line = katz_deli[0]

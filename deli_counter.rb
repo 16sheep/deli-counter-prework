4b4d43a33a3e3768c790a5901f13691f8ed53e02
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-09 09:05:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,10 @@ def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.length < 0
+    puts 
+  first_in_line = katz_deli[0]
+  "Currently serving #{first_in_line}."
+end

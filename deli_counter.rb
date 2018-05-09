d16e1dcebc84af6f43eae45cfcee4e35b1dfc044
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-09 08:59:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.length > 0
     line_string = "The line is currently: "
     katz_deli.each_with_index {|name, index | 
-      line_string = line_string + "#{index + 1}. #{name} "
+      line_string = line_string + "#{index + 1}. #{name} ".strip
     }
     puts line_string
   else 
